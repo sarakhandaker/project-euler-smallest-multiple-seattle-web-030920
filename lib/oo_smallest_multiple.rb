565c3d8c13e61e73891a4c45376eb6c41151b45a
SaraKhandaker <kttsara@gmail.com>
2020-07-21 16:10:01 -0700

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+    def initialize(input)
+        @input=input
+    end
+
+    def lcm
+        placeholder = 1
+        (2..@input).each {|i| placeholder *= i / gcd(placeholder, i) }
+        placeholder
+    end
+          
+    def gcd(a, b)
+        while b > 0
+            a %= b
+        return b if a == 0
+            b %= a
+        end
+        a
+    end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,47 @@
-# Implement your procedural solution here!
+# # Implement your procedural solution here!
+# def smallest_multiple(input)
+#     array=[]
+#     (2..input).each do |i|
+#         array.push(i)
+#     end
+
+#     keeplooking=true
+
+#     number=input
+
+#     while keeplooking
+
+#         good=true
+
+#         array.each do |i|
+#             if number%i !=0
+#                 good=false
+#             end
+#         end
+
+#         if good 
+#             keeplooking=false
+#         else 
+#             number+=1
+#         end
+        
+#     end
+#     number
+# end
+
+def smallest_multiple(input)
+    placeholder = 1
+    (2..input).each do |i|
+      placeholder *= i / gcd(placeholder, i)
+    end
+    placeholder
+  end
+  
+  def gcd(a, b)
+    while b > 0
+      a %= b
+      return b if a == 0
+      b %= a
+    end
+    a
+  end
